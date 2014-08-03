52bb79442c626900c0712e6efe7ec70bca3b520b
jongyoon lim <seedengine@gmail.com>
2014-08-03 22:25:46 +0900

    add player

---
+++ web/js/player.js
@@ -0,0 +1,45 @@
+
+var player = new function(youtubePlayer) {
+    var ytPlayer = youtubePlayer;
+    var songList = new playList();
+    var playOrderList = new Arrays();
+    var repeat.type = ["NONE", "ONE", "ALL"];
+    var shuffle = false;
+
+    var play = function() {
+
+    };
+
+    var stop = function() {
+
+    }
+
+    var resume = function() {
+
+    }
+
+    var next = function() {
+
+    }
+
+    var prev = function() {
+
+    }
+
+    var repeat = function() {
+
+    }
+
+    var shuffle = function() {
+
+    }
+
+    var addSongsToPlayList = function() {
+
+    }
+
+    var removeSongsFromPlayList = function() {
+
+    }
+
+}
